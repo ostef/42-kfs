5d5b0963f507cc38febb16e4ca85006928733279
Stefan <oumansour.stefan@gmail.com>
2026-02-05 13:22:06 +0100

    Rename vga_color to vga_color_t

--- Source/vga.h
+++ Source/vga.h
@@ -7,7 +7,7 @@
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((uint16_t*) 0xB8000)
 
-enum vga_color {
+typedef enum vga_color_t {
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
 	VGA_COLOR_GREEN = 2,
@@ -24,19 +24,19 @@ enum vga_color {
 	VGA_COLOR_LIGHT_MAGENTA = 13,
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
-};
+} vga_color_t;
 
 extern uint8_t g_tty_color;
 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+static inline uint8_t vga_entry_color(enum vga_color_t fg, enum vga_color_t bg) {
 	return fg | bg << 4;
 }
 
-static inline uint8_t vga_entry_color_fg(enum vga_color fg) {
+static inline uint8_t vga_entry_color_fg(enum vga_color_t fg) {
 	return vga_entry_color(fg, g_tty_color >> 4);
 }
 
-static inline uint8_t vga_entry_color_bg(enum vga_color bg) {
+static inline uint8_t vga_entry_color_bg(enum vga_color_t bg) {
 	return vga_entry_color(g_tty_color & 0x0F, bg);
 }
 
