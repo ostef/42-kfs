61004f25378e12a045868dab08e5ec61626e4332
soumanso <soumanso@vbox.42lyon.fr>
2026-02-09 14:54:12 +0100

    Fixed incorrect values for KERNEL_DATA_SEGMENT (again)

--- Source/interrupt_handlers.asm
+++ Source/interrupt_handlers.asm
@@ -2,8 +2,8 @@
 [extern handle_isr]
 [extern handle_irq]
 
-KERNEL_CODE_SEGMENT equ 0x08
-KERNEL_DATA_SEGMENT equ 0x16
+KERNEL_CODE_SEGMENT equ 8
+KERNEL_DATA_SEGMENT equ 16
 
 isr_common_stub:
 	pusha
