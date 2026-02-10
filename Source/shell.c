0383821bbc44d957f02dd81d55baf3d1701a01e5
Stefan <oumansour.stefan@gmail.com>
2026-02-10 16:50:16 +0100

    Added gdtdump command

--- Source/shell.c
+++ Source/shell.c
@@ -159,7 +159,7 @@ static void get_next_arg(const char *buff, k_size_t buff_len, k_size_t *inout_of
 }
 
 void shell_print_help() {
-	k_printf("Commands: help, clear, echo [args...], stackdump, shutdown, reboot\n");
+	k_printf("Commands: help, clear, echo [args...], stackdump, gdtdump, shutdown, reboot\n");
 }
 
 void shell_loop() {
@@ -194,6 +194,14 @@ void shell_loop() {
 			shell_print_help();
 		} else if (cmd_len >= k_strlen("stackdump") && k_strncmp(cmd, "stackdump", cmd_len) == 0) {
 			k_print_stack();
+		} else if (cmd_len >= k_strlen("gdtdump") && k_strncmp(cmd, "gdtdump", cmd_len) == 0) {
+			uint32_t *gdt = (uint32_t *)0x800;
+
+			int i = 0;
+			while (i < (8 * 7) / 4) {
+				k_printf("Entry %i at %p: %p %p\n", i / 2, gdt + i, gdt[i], gdt[i + 1]);
+				i += 2;
+			}
 		} else if (cmd_len >= k_strlen("shutdown") && k_strncmp(cmd, "shutdown", cmd_len) == 0) {
 			k_printf("Shutting down\n");
 			ioport_write_word(0x604, 0x2000);
